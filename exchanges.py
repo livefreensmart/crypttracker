ba99a6ac91edbef957ce67ee211467cd266010d0
Yam <livefreensmart@users.noreply.github.com>
2018-09-20 21:36:20 -0500

    List of exchange accounts
    Manually generated from lukestokes' exchange report

---
+++ exchanges.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 20 11:28:59 2018
+
+@author: lovenfreedom
+"""
+
+EXCHANGES = [
+        'binance',
+        'binance-hot',
+        'bittrex',
+        'blocktrades',
+        'coinpayments.net',
+        'deepcrypto8',
+        'freewallet',
+        'freewallet.org',
+        'gopax',
+        'gopax-deposit',
+        'hitbtc',
+        'hitbtc-exchange',
+        'hitbtc-payout',
+        'huobi',
+        'huobi-pro',
+        'huobi-withdrawal',
+        'myupbit',
+        'openledger',
+        'openledger-dex',
+        'poloniex',
+        'rudex',
+        'steemexchanger',
+        'upbit',
+        'upbit-exchange',
+        'upbit-steemhot',
+        'upbituserwallet'        
+        ]
