a3cbe16c6e8cba65e8fc168a3ac6a139e4bc189f
crokkon <33018033+crokkon@users.noreply.github.com>
2018-11-07 22:16:44 +0100

    Rewrite using beem's account history and click
    * use beem's `Account.history_reverse()`, fixes #1
    * adding `detective.py` as click CLI interface
    * adding `memomatch` to find identical exchange transfer memos between
    accounts
    * adding `transfermatch` to analyze connections between two accounts
    * adding `accountmatch` to find similar account names
    * update requirements - all downstream components are handled by beem's
    requirements.txt

---
+++ detective.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 07 2018
+
+@author: crokkon
+
+"""
+from __future__ import print_function
+import click
+import crypttracker as ct
+
+
+@click.group(chain=True)
+@click.option(
+    '--verbose', '-v', default=False, is_flag=True,
+    help='Enable verbose output')
+@click.pass_context
+def cli(ctx, verbose):
+    """crypttracker CLI app
+
+    """
+    ctx.obj['DEBUG'] = verbose
+    pass
+
+
+@cli.command()
+@click.argument('account1')
+@click.argument('account2')
+@click.option('--days', '-d', default=0,
+              help='Restrict transfers to the last [days] days'
+              '(0: no limit, default)')
+@click.pass_context
+def transfermatch(ctx, account1, account2, days):
+    """Investigate the connection between two accounts
+
+    """
+    print(ct.transfermatch(account1, account2, days=days,
+                           debug=ctx.obj['DEBUG']))
+
+
+@cli.command()
+@click.argument('account')
+@click.option('--days', '-d', default=0,
+              help='Restrict transfers to the last [days] days'
+              '(0: no limit, default)')
+@click.option('--trx-type', '-t', type=click.Choice(['in', 'out', 'all']),
+              default='all', help="Limit transaction type. Can be 'in', "
+              "'out', or 'all' (default)")
+@click.pass_context
+def transfers(ctx, account, days, trx_type):
+    """List transfers sent or received by ACCOUNT.
+
+    """
+    print(ct.transfers(account, days=days, trx_type=trx_type,
+                       debug=ctx.obj['DEBUG']))
+
+
+@cli.command()
+@click.argument('exchange')
+@click.argument('memo')
+@click.option('--days', '-d', default=0,
+              help='Restrict transfers to the last [days] days'
+              '(0: no limit, default)')
+@click.pass_context
+def memomatch(ctx, exchange, memo, days):
+    """Find exchange transfers matching a given memo.
+
+    MEMO can be a full transfer memo or a substring.
+
+    """
+    print(ct.memomatch(exchange, memo, days=days,
+                       debug=ctx.obj['DEBUG']))
+
+
+@cli.command()
+@click.argument('account')
+@click.option('--limit', '-l', default=10,
+              help='limit the output to [limit] results (default: 10)')
+@click.pass_context
+def accountmatch(ctx, account, limit):
+    """Find account names similar to the provided string.
+
+    ACCOUNT can be an existing account or a partial name. Limit the
+    output to [limit] results.
+
+    """
+    print(", ".join(ct.accountmatch(account, limit)))
+
+
+@cli.command()
+@click.argument('account')
+@click.option('--days', '-d', default=0,
+              help='Restrict transfers to the last [days] days '
+              '(0: no limit, default)')
+@click.pass_context
+def exchangetransfers(ctx, account, days):
+    """Find all transfers to exchanges from a given account.
+
+    Lists all transfers from a given account to any of the known
+    exchanges within the last [days] days (default: no limit).
+
+    """
+    print(ct.exchangetransfers(account, days=days,
+                               debug=ctx.obj['DEBUG']))
+
+
+if __name__ == "__main__":
+    cli(obj={})
